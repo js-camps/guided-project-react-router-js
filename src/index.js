78f30e527b160623315432304378f4916ece125b
rjgeng <rj.geng@gmail.com>
2024-07-11 09:14:16 -0700

    add task steps

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { createRoot } from "react-dom/client";
 import  App  from "./components/App";
 
+// Importing the top-level component
+import App from './components/App'
+
+// Importing the styles
 import './styles.less'
+
+// 👉 STEP 1 - Import React Router's Router
 
 const container = document.getElementById("app");
 const root = createRoot(container)
-root.render(<App />);
+root.render(
+    <App />
+    );
